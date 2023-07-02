b4019229b50f968e4425a54bbda98b2636f3557b
FrancooChaparro <francoo_chaparro@hotmail.com>
2023-07-02 12:33:26 -0300

    delete, paramsID, QueryUsers, GetAllUsers

--- backeco/src/routes/UserRouter.ts
+++ backeco/src/routes/UserRouter.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { UserCreate, UserPut } from "../controllers/createUser";
+import { userCreate, userPut, getUsers, getUserParams, deleteUser } from "../controllers/createUser";
 
 export const UserRouter = Router();
 
 
-UserRouter.post("/create", UserCreate)
-UserRouter.post("/put/:id", UserPut)
+UserRouter.post("/create", userCreate)
+UserRouter.post("/put/:id", userPut)
+UserRouter.get("/", getUsers)
+UserRouter.get("/:id", getUserParams)
+UserRouter.delete("/delete/:id", deleteUser)
