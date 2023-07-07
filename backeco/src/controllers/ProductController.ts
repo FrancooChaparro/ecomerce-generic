7f1a27b8e5e37e5fe94ad33590f9e6e3ca978c85
FrancooChaparro <francoo_chaparro@hotmail.com>
2023-07-07 01:40:01 -0300

    finish_day

--- backeco/src/controllers/ProductController.ts
+++ backeco/src/controllers/ProductController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { product } from "../../db";
+import { Op } from "sequelize";
 
 export const createProduct = async (req: Request, res: Response) => {
   try {
@@ -32,17 +33,20 @@ export const productPut = async (req: Request, res: Response) => {
     const { id } = req.params;
     const { name, price, description, stock } = req.body;
     console.log(req.body);
-    
+
     const productToUpdate = await product.findByPk(id);
 
-    if (!productToUpdate) throw new Error("El producto que desea actualizar no existe");
+    if (!productToUpdate)
+      throw new Error("El producto que desea actualizar no existe");
 
-    if (!name || !price || !description || !stock) return res.json({ msg: "Missing required fields" });
+    if (!name && !price && !description && !stock)
+      return res.json({ msg: "Missing required fields" });
 
-    const update = await product.update(
+    await product.update(
       { name, price, description, stock },
       { where: { id } }
     );
+    const update = await product.findByPk(id);
 
     return res.status(200).json({
       msg: `Product update successfully`,
@@ -55,5 +59,81 @@ export const productPut = async (req: Request, res: Response) => {
     }
     return res.json({ msg: `Error 404 - Unknown error occurred` });
   }
-  }
+};
 
+export const getProducts = async (req: Request, res: Response) => {
+  const { name } = req.query;
+  const regex_FullText = /^([a-zA-Z ]+)/i;
+
+  try {
+    let products;
+
+    if (typeof name === "string" && name.trim() !== "") {
+      if (regex_FullText.test(name)) {
+        products = await product.findAll({
+          where: {
+            name: { [Op.iLike]: `%${name}%` },
+          },
+        });
+
+        if (products.length === 0) {
+          return res.status(500).json({
+            status: false,
+            msg: `No se encontró ningún producto con el atributo '${name}'`,
+            errorCode: 12,
+          });
+        }
+      } else {
+        return res.status(500).json({
+          status: false,
+          msg: `Formato de búsqueda inválido`,
+          errorCode: 14,
+        });
+      }
+    } else {
+      products = await product.findAll();
+    }
+
+    return res.status(200).json({ data: products, message: "Listado de productos" });
+  } catch (error) {
+    return res.status(400).json({
+      status: false,
+      msg: `Error en la solicitud: ${error}`,
+      errorCode: 400,
+    });
+  }
+};
+
+
+export const getProductParams = async (req: Request, res: Response) => { 
+  const productID = req.params.id;
+  try {
+    const result = await product.findByPk(productID);
+    if (result) {
+      res.status(200).json({ data: result, message: "Producto solicitado" });
+    } else {
+      res
+        .status(404)
+        .json({ error: `No existe un usuario con el ID ${productID}` });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+}
+
+
+export const deleteProduct = async (req: Request, res: Response) => { 
+  const { id } = req.params;
+
+  try {
+    const deletedProduct = await product.destroy({
+      where: {
+        id: `${id}`,
+      },
+    });
+    if (!deletedProduct) return res.json({ msg: "product does not exist" });
+    return res.json({ msg: "product Deleted" });
+  } catch (error) {
+    return res.json({ msg: `Error 404 - ${error}` });
+  }
+}
--- backeco/src/routes/ProductRouter.ts
+++ backeco/src/routes/ProductRouter.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { createProduct, productPut } from "../controllers/ProductController";
+import { createProduct, productPut, getProducts, getProductParams, deleteProduct } from "../controllers/ProductController";
 
 export const ProductRouter = Router();
 
 
 ProductRouter.post("/create", createProduct)
 ProductRouter.put("/put/:id", productPut)
+ProductRouter.get("/", getProducts)
+ProductRouter.get("/:id", getProductParams)
+ProductRouter.delete("/delete/:id", deleteProduct)
